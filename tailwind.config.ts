2753f5098d990585ad85ac47bb78cb29a8f76177
hamzabell <152490531+abassey16@users.noreply.github.com>
2024-04-21 00:37:52 +0100

    feat: 🎸 initialize RabIT social application

---
+++ tailwind.config.ts
@@ -0,0 +1,19 @@
+import { type Config } from "tailwindcss";
+import { fontFamily } from "tailwindcss/defaultTheme";
+
+export default {
+  content: [
+    "./src/**/*.tsx", 
+    "./node_modules/flowbite-react/lib/**/*.js"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-sans)", ...fontFamily.sans],
+      },
+    },
+  },
+  plugins: [
+    require("flowbite/plugin")
+  ],
+} satisfies Config;
